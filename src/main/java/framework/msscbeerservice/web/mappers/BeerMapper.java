decf8709faa4e3b4d2affe6b031f3334d2db4214
Kaiotech <113994695+Kaiotech@users.noreply.github.com>
2022-10-09 18:41:39 +0200

    Adding MapStruct - closes #10

---
+++ src/main/java/framework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,14 @@
+package framework.msscbeerservice.web.mappers;
+
+
+import framework.msscbeerservice.domain.Beer;
+import framework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
---
+++ target/generated-sources/annotations/framework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,37 @@
+package framework.msscbeerservice.web.mappers;
+
+import framework.msscbeerservice.domain.Beer;
+import framework.msscbeerservice.web.model.BeerDto;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-09T18:31:23+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.3 (Amazon.com Inc.)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        return beerDto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        return beer;
+    }
+}
