cd3d3d451f339f37434ced72572029ff19c21b49
Kaiotech <113994695+Kaiotech@users.noreply.github.com>
2022-10-10 12:43:25 +0200

    Adding MapStruct - bugfix

--- target/generated-sources/annotations/framework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/framework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-09T18:31:23+0200",
+    date = "2022-10-09T18:48:45+0200",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.3 (Amazon.com Inc.)"
 )
 @Component
