afdbab17566474b0f55047814c93f3c8050575a8
Kaiotech <kaiotech@icloud.com>
2022-10-30 14:19:30 +0100

    Added brewing and listener

--- target/generated-sources/annotations/framework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/framework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-25T18:08:40+0200",
+    date = "2022-10-30T14:04:24+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.3 (Amazon.com Inc.)"
 )
 @Component
