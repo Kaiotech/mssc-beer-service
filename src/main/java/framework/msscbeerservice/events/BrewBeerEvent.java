adc64e3f7911d2776db8e49ff87b4f1957d1856f
Kaiotech <kaiotech@icloud.com>
2022-10-30 14:30:02 +0100

    fixed Jackson config - broken code/fix later

--- src/main/java/framework/msscbeerservice/events/BrewBeerEvent.java
+++ src/main/java/framework/msscbeerservice/events/BrewBeerEvent.java
@@ -1,7 +1,9 @@
 package framework.msscbeerservice.events;
 
 import framework.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class BrewBeerEvent extends BeerEvent {
 
     public BrewBeerEvent(BeerDto beerDto) {
